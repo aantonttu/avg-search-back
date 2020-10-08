5b60e4f4a8b76cd62c224bfde913a1c34b4470c3
aanton@ttu.ee <Pashtet1234>
2020-10-09 00:34:17 +0300

    application.yaml changed for h2 db

--- src/main/java/ee/taltech/team24backend/MoviesApplicationInit.java
+++ src/main/java/ee/taltech/team24backend/MoviesApplicationInit.java
@@ -1,34 +1,34 @@
-package ee.taltech.team24backend;
-
-import ee.taltech.team24backend.model.Movie;
-import ee.taltech.team24backend.repository.MovieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-@Component
-public class MoviesApplicationInit implements CommandLineRunner {
-
-    @Autowired
-    private MovieRepository movieRepository;
-
-    @Override
-    public void run(String... args) throws Exception {
-        List<Movie> movies = List.of(
-                new Movie("Antlers", "Description", "Scott Cooper", 9.8f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
-                new Movie("Candyman", "Description", "Nia DaCosta", 6.3f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
-                new Movie("Ghostbusters: Afterlife", "Description", "Jason Reitman", 7.5f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
-                new Movie("Halloween Kills", "Description", "David Gordon Green", 9.5f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
-                new Movie("Morbius", "Description", "Daniel Espinosa", 7.9f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
-                new Movie("Spiral", "Description", "Darren Lynn Bousman", 8.1f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
-                new Movie("The Forever Purge", "Description", "Everardo Gout", 3.7f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
-                new Movie("Wendell and Wild", "Description", "Henry Selick", 4.5f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
-                new Movie("Halloween Ends", "Description", "David Gordon Green", 6.8f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
-                new Movie("Dark Harvest", "Description", "David Slade", 9.1f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
-                new Movie("Escape Room 2", "Description", "Adam Robitel", 9.7f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "comedy")
-        );
-        movieRepository.saveAll(movies);
-    }
-}
+//package ee.taltech.team24backend;
+//
+//import ee.taltech.team24backend.model.Movie;
+//import ee.taltech.team24backend.repository.MovieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+//@Component
+//public class MoviesApplicationInit implements CommandLineRunner {
+//
+//    @Autowired
+//    private MovieRepository movieRepository;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        List<Movie> movies = List.of(
+//                new Movie("Antlers", "Description", "Scott Cooper", 9.8f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
+//                new Movie("Candyman", "Description", "Nia DaCosta", 6.3f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
+//                new Movie("Ghostbusters: Afterlife", "Description", "Jason Reitman", 7.5f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
+//                new Movie("Halloween Kills", "Description", "David Gordon Green", 9.5f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
+//                new Movie("Morbius", "Description", "Daniel Espinosa", 7.9f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
+//                new Movie("Spiral", "Description", "Darren Lynn Bousman", 8.1f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
+//                new Movie("The Forever Purge", "Description", "Everardo Gout", 3.7f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
+//                new Movie("Wendell and Wild", "Description", "Henry Selick", 4.5f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
+//                new Movie("Halloween Ends", "Description", "David Gordon Green", 6.8f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
+//                new Movie("Dark Harvest", "Description", "David Slade", 9.1f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "thriller"),
+//                new Movie("Escape Room 2", "Description", "Adam Robitel", 9.7f, "https://cs7.pikabu.ru/post_img/big/2019/05/23/9/1558626426166757449.jpg", "comedy")
+//        );
+//        movieRepository.saveAll(movies);
+//    }
+//}
--- src/main/java/ee/taltech/team24backend/repository/MovieRepository.java
+++ src/main/java/ee/taltech/team24backend/repository/MovieRepository.java
@@ -1,9 +1,12 @@
 package ee.taltech.team24backend.repository;
 
 import ee.taltech.team24backend.model.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
 }
