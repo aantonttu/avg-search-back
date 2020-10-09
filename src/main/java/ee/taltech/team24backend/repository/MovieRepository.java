b5f6dacafe3f6b52387afaba6db9176bf96dfb89
aanton@ttu.ee <Pashtet1234>
2020-10-10 00:52:45 +0300

    Flyway for db is set up - created queries for main schema and "movies" table.

--- src/main/java/ee/taltech/team24backend/repository/MovieRepository.java
+++ src/main/java/ee/taltech/team24backend/repository/MovieRepository.java
@@ -1,9 +1,7 @@
 package ee.taltech.team24backend.repository;
 
 import ee.taltech.team24backend.model.Movie;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
