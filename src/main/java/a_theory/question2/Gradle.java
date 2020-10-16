4b79e012ff4b05fcd3c6818ebae49886b077fa90
German Hanmamedov <gehanm@ttu.ee>
2020-10-16 20:03:44 +0300

    question 2

--- src/main/java/a_theory/question2/Gradle.java
+++ src/main/java/a_theory/question2/Gradle.java
@@ -9,20 +9,22 @@ public class Gradle {
     //todo p1
     // In your words (do not use wiki definitions)
     // What is gradle?
-    // Answer:
+    // Answer: It is build automation tool, so we have to only add dependency in build.gradle file and gradle will it
+    // automatically add it to the project.
 
     //todo p2
     // What is gradle wrapper? Why are we using it instead of gradle?
-    // Answer:
+    // Answer: Gradle wrapper is used to run build without locally installed gradle. In my opinion, we are using gradle
+    // wrapper in case to run and build our projects on different machines without installing gradle locally on each.
 
     //todo p3
     // Name at least 1 alternative to gradle
-    // 1
+    // 1 Apache Maven
 
     //todo p4
     // Name 3 different benefits or reasons for using gradle
-    // 1
-    // 2
-    // 3
+    // 1 Dependency management
+    // 2 Easy to integrate
+    // 3 Easy to use
 
 }
