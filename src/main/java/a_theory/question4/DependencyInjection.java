3a60b79eca42e8947071be7c610fbcbea30b0603
subot <vlpolj@ttu.ee>
2020-10-16 18:12:44 +0300

    Theory: Question 4

--- src/main/java/a_theory/question4/DependencyInjection.java
+++ src/main/java/a_theory/question4/DependencyInjection.java
@@ -9,18 +9,19 @@ public class DependencyInjection {
     //todo p1
     // In your words (do not use wiki definitions)
     // What is Dependency Injection?
-    // Answer:
+    // Answer: A way not to create classes manually, but use beans for that
 
     //todo p2
     // Bring example from your code of Dependency Injection.
-    // Paste your code here, but comment it out
+    // Paste your code here, but comment it out (we dont have that in our code)
 
     //todo p3
     // Name 2 benefits of Dependency Injection
-    // 1
-    // 2
+    // 1 if we want change dependency in class, we doesn't need to change every place where we use it,
+    // enough to change beans in configuration file
+    // 2 testing can be performed using mock objects
 
     //todo p4
     // Name 1 disadvantage of Dependency Injection
-    // 1
+    // 1 DI can encourage dependence on DI framework.
 }
