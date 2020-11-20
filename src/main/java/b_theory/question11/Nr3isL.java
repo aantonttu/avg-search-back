cfea13f8ef65e674e2a64e8f66fa74e8bc99a5bb
subot <vlpolj@ttu.ee>
2020-11-20 20:55:48 +0200

    Question 11 L

--- src/main/java/b_theory/question11/Nr3isL.java
+++ src/main/java/b_theory/question11/Nr3isL.java
@@ -1,6 +1,5 @@
 package b_theory.question11;
 
-import math;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,51 +15,51 @@ public class Nr3isL {
     // B -> People creation example (lets play God role)
     // We have standard class of person creation and class of black person creation(niga)
     // Black_person is subtype of Person and all black_person objects can be replaced with whit.. just simple person objects.
+    // example with pseudo code
 
-
-    private class Person {
-        private int height = random(50, 250);
-        private int beauty = random(0, 100);
-        private int iq = random(60, 200);
-
-        private String eyes_color = random('green', 'blue', 'brown');
-        private String hair_color = random('blond', 'black', 'brown');
-        private String skin_color = random('white, black, yelow');
-
-        private String name;
-
-        private void setName(String name) {
-            this.name = name;
-        }
-
-        private List<Object> get_person_properties() {
-            List<Object> properites = new ArrayList<>();
-            properites.addAll(this.height, this.beauty, this.iq, this.eyes_color, this.hair_color, this.skin_color, this.name);
-            return properites;
-        }
-    }
-
-    private class Black_Person {
-        private int height = random(200, 350);
-        private int beauty = random(0, 100);
-        private int iq = random(60, 200);
-
-        private String eyes_color = random('brown');
-        private String hair_color = random('black');
-        private String skin_color = random('black');
-
-        private String name;
-
-        private void setName(String name) {
-            this.name = name;
-        }
-
-        private List<Object> get_person_properties() {
-            List<Object> properites = new ArrayList<>();
-            properites.addAll(this.height, this.beauty, this.iq, this.eyes_color, this.hair_color, this.skin_color, this.name);
-            return properites;
-        }
-    }
+//    private class Person {
+//        private int height = random(50, 250);
+//        private int beauty = random(0, 100);
+//        private int iq = random(60, 200);
+//
+//        private String eyes_color = random('green', 'blue', 'brown');
+//        private String hair_color = random('blond', 'black', 'brown');
+//        private String skin_color = random('white, black, yelow');
+//
+//        private String name;
+//
+//        private void setName(String name) {
+//            this.name = name;
+//        }
+//
+//        private List<Object> get_person_properties() {
+//            List<Object> properites = new ArrayList<>();
+//            properites.addAll(this.height, this.beauty, this.iq, this.eyes_color, this.hair_color, this.skin_color, this.name);
+//            return properites;
+//        }
+//    }
+//
+//    private class Black_Person {
+//        private int height = random(200, 350);
+//        private int beauty = random(0, 100);
+//        private int iq = random(60, 200);
+//
+//        private String eyes_color = random('brown');
+//        private String hair_color = random('black');
+//        private String skin_color = random('black');
+//
+//        private String name;
+//
+//        private void setName(String name) {
+//            this.name = name;
+//        }
+//
+//        private List<Object> get_person_properties() {
+//            List<Object> properites = new ArrayList<>();
+//            properites.addAll(this.height, this.beauty, this.iq, this.eyes_color, this.hair_color, this.skin_color, this.name);
+//            return properites;
+//        }
+//    }
 
 
 }
