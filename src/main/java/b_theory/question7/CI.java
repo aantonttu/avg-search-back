314cb900230967bbb0978932b2ac08bd4d44ea9c
German Hanmamedov <gehanm@ttu.ee>
2020-11-20 20:24:36 +0300

    q7

--- src/main/java/b_theory/question7/CI.java
+++ src/main/java/b_theory/question7/CI.java
@@ -4,28 +4,28 @@ public class CI {
 
     //todo A
     // What is integration?
-    // Answer:
+    // Answer: When smaller component work together as a single system.
 
     //todo B
     // What makes integration continuous?
-    // Answer:
+    // Answer: Integration into shared repo code by several developers with automated build, test stages pipeline.
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick [x] which ones stand for continuous integration
-    // [] compile
-    // [] test
+    // [X] compile
+    // [X] test
     // [] deploy
 
     //todo D
     // Name and explain 1 benefit of CI
-    // 1
+    // 1 Bugs could be detected earlier and easier to track, which saves time and money.
 
     //todo E
     // Name and explain 1 drawback of CI
-    // 1
+    // 1 Legacy systems may not support CI technologies.
 
     //todo F
     // In the class we are using gitlab-ci. Name 2 alternatives to it
-    // 1
-    // 2
+    // 1 Jenkins
+    // 2 Bamboo
 }
