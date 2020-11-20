9d875e1414f407eb090440cc504d37cb4d9dea1b
Anton Antonov <Pashtet1234>
2020-11-20 21:33:45 +0200

    O in SOLID

--- src/main/java/b_theory/question11/Nr2isO.java
+++ src/main/java/b_theory/question11/Nr2isO.java
@@ -3,6 +3,31 @@ package b_theory.question11;
 public class Nr2isO {
 
     //todo this is a contribution based question so make sure to keep commits separate
-    //todo A What does O stand for in SOLID? Explain the principle.
+    //todo A What does O stand for in SOLID? Explain the principle. Answer: O stands for open-closed principle, it means /
+    // that entities should be open for extension and closed for modification.
     //todo B Give an example. Write actual or pseudo code.
 }
+//pseudo code
+class VolumeCalc{
+    private float volume() {
+        //returns volume
+        return 0;
+    }
+}
+class CubeVolCalc{
+    double x;
+    double y;
+    double z;
+    //calculates cube volume
+    public  double volume(){
+        return x*y*z;
+    }
+}
+class SphereVolCalc{
+    double r;
+    //calculates cube volume
+    public double volume(){
+        return 4/3*(3.14*r*r*r);
+    }
+}
+// Other figures can be added to calculate volume
