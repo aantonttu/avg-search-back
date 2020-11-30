e481fa2f0fb56bf1bb902d2eb4b2153be75ad2c7
Timo Loomets <tiloom@ttu.ee>
2020-11-30 20:25:32 +0200

    Grading: Test commit. (You can revert this later)

--- src/main/java/ee/taltech/team24backend/controller/MoviesController.java
+++ src/main/java/ee/taltech/team24backend/controller/MoviesController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("movies")
+@RequestMapping({"movies", "movies2"})
 @RestController
 public class MoviesController {
 
