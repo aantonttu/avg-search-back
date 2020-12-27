3bd2ed8602179c60ba787c0c2844e2ba17588d58
Anton Antonov <aanton@ttu.ee>
2020-12-27 14:20:03 +0000

    Fixed problem with swagger and h2-console being unreachable.

--- src/main/java/ee/taltech/team24backend/security/SecurityConfig.java
+++ src/main/java/ee/taltech/team24backend/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -50,11 +51,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers(HttpMethod.GET, "/movies/**").permitAll()
-                .antMatchers("/swagger-ui/**").permitAll()
                 .antMatchers("/users/register").permitAll()
                 .antMatchers("/users/login").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().fullyAuthenticated()
+                .and()
+                .csrf().ignoringAntMatchers("/h2-console/**")
         ;
+    }
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html");
     }
 
 
