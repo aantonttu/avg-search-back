3dbbf346fe68cfd081f0c214ec1fa942c78fa729
Vladislav Poljakov <vlpolj@ttu.ee>
2020-12-30 12:21:17 +0200

    c_theory - question14 - phones

--- src/main/java/c_theory/question14/phones/PhonesController.java
+++ src/main/java/c_theory/question14/phones/PhonesController.java
@@ -1,5 +1,12 @@
 package c_theory.question14.phones;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("phones")
+@RestController
 public class PhonesController {
 
     //todo for question 14 there are 4 assignments in total
@@ -18,18 +25,49 @@ public class PhonesController {
     //todo A add necessary annotations on the class
 
     //todo B create a method to query phones (plural)
+    @GetMapping
+    public List<Phone> getPhones(@RequestParam(value = "manufacturer", required = false) String manufacturer,
+                                 @RequestParam(value = "from", required = false) Integer from,
+                                 @RequestParam(value = "to", required = false) Integer to,
+                                 @RequestParam(value = "by", defaultValue = "popular") String by,
+                                 @RequestParam(value = "order", defaultValue = "desc") String order) {
+        return new ArrayList<>();
+    }
 
     //todo C create a method to query single phone
+    @GetMapping("{name}")
+    public Phone getPhone(@PathVariable String name) {
+        return new Phone();
+    }
 
     //todo D create a method to save a phone
+    @PostMapping()
+    public Phone savePhone(@RequestBody Phone phone) {
+        return phone;
+    }
 
     //todo E create a method to update a phone
+    @PutMapping("{name}")
+    public Phone updatePhone(@RequestBody Phone phone, @PathVariable String name) {
+        return phone;
+    }
 
     //todo F create a method to delete a phone
+    @DeleteMapping("{name}")
+    public void deletePhone(@PathVariable String name) {
+    }
 
     //todo G assuming each phone has apps installed (one-to-many relation) create a method to query phone's apps
+    @GetMapping("{name}/apps")
+    public List<App> getPhoneApps(@PathVariable String name) {
+        return new ArrayList<>();
+    }
 
     //todo H create a method to update phone's price (and nothing else)
+    @PutMapping("{name}/{price}")
+    public Phone updatePhonePrice(@PathVariable String name, @PathVariable String price) {
+        return new Phone();
+    }
 
     //todo I modify correct method to support searching by manufacturer while keeping original functionality
 
