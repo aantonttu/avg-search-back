cbd6a8f1ab9e358de8539208d3482e501277267a
Anton Antonov <Pashtet1234>
2020-12-30 03:10:28 +0200

    Theory

--- src/main/java/c_theory/question13/MaterialDesign.java
+++ src/main/java/c_theory/question13/MaterialDesign.java
@@ -4,10 +4,12 @@ public class MaterialDesign {
 
     //todo A
     // What is material design?
-    // Answer:
+    // Answer: It is graphical design style. It uses more animations, and graphical effects like shadows and lightning.
+
 
     //todo B
     // Name and describe 2 principles of material design (there are more than 2)
-    // 1
-    // 2
+    // 1 Things should look like things from real world.
+    // As an example pop-ups should be with shadow, because real 3D objects are with shadows.
+    // 2 Objects should move, it provides user feedback and makes things easier to understand.
 }
--- src/main/java/c_theory/question14/lessons/LessonsController.java
+++ src/main/java/c_theory/question14/lessons/LessonsController.java
@@ -1,6 +1,14 @@
 package c_theory.question14.lessons;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("lessons")
+@RestController
 public class LessonsController {
+
 
     //todo for question 14 there are 4 assignments in total
     // Each person has to do only 1. So 2 person team has to do 2 different ones, 3 person - 3, 4 person - 4.
@@ -15,30 +23,61 @@ public class LessonsController {
     // Method body is not important and will not be graded.
     // Modifying other classes is unnecessary and will not be graded.
 
-    //todo A add necessary annotations on the class
+    //todo A add necessary annotations on the class +
 
     //todo B create a method to query lessons (plural)
+    @GetMapping
+    public List<Lesson> getLessons(@RequestParam(value = "course", required = false) Long course,
+                                  @RequestParam(value = "year", required = false, defaultValue = "2020") Long year,
+                                   @RequestParam(value = "order", defaultValue = "predefined") String order) {
+        //order = asc/desc
+        return new ArrayList<>();
+    }
 
     //todo C create a method to query single lesson
+    @GetMapping("{name}")
+    public Lesson getLesson(@PathVariable String name) {
+        return new Lesson();
+    }
+
 
     //todo D create a method to save a lesson
+    @PostMapping()
+    public Lesson saveLesson(@RequestBody Lesson lesson) {
+        return lesson;
+    }
 
     //todo E create a method to update a lesson
+    @PutMapping("{name}")
+    public Lesson updateLesson(@PathVariable String name, @RequestBody Lesson lesson) {
+        return lesson;
+    }
 
     //todo F create a method to delete a lesson
+    @DeleteMapping("{name}")
+    public void deleteComment(@PathVariable String name) {
+    }
 
     //todo G assuming each Lesson has students (one-to-many relation) create a method to query lesson's students
+    @GetMapping("{name}/students")
+    public List<Students> getStudents(@PathVariable String name) {
+        return new ArrayList<>();
+    }
 
     //todo H create a method to update lesson's name (and nothing else)
+    @PutMapping("{name1}/to/{name2}")
+    public Lesson updateLesson(@PathVariable String name1, @PathVariable String name2) {
+        return new Lesson();
+    }
 
-    //todo G modify correct method to support searching lessons by course id while keeping original functionality
+    //todo G modify correct method to support searching lessons by course id while keeping original functionality +
 
     //todo H modify correct method to support searching by year with default being current year (2020)
-    // (you can ignore semesters or use year-semester string)
+    // (you can ignore semesters or use year-semester string) +
 
     //todo K modify correct method to order lessons
     // * by most visitors first
     // * by least visitors first
-    // (you can assume that by default it searches by predefined lecturer's order)
+    // (you can assume that by default it searches by predefined lecturer's order) +
 
 }
