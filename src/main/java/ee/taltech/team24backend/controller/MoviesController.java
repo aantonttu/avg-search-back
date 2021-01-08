2ee960d666c146cbccac9212a4ab495f659b46a6
Tanel Ehatamm <tanel.ehatamm@ttu.ee>
2021-01-08 21:13:14 +0000

    Revert "Test commit p3"
    This reverts commit 5e0379931bf0ad03ea9a77ccd9477b25e7f4f209

--- src/main/java/ee/taltech/team24backend/controller/MoviesController.java
+++ src/main/java/ee/taltech/team24backend/controller/MoviesController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping({"movies", "movies2"})
+@RequestMapping("movies")
 @RestController
 public class MoviesController {
 
